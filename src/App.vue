ba19c5b64165875f6afdc95e5e57417547f3e5c7
keithmchd48 <keithmchd48@gmail.com>
2021-08-28 17:42:35 +0530

    navigation completed

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,602 @@
 <template>
-<div></div>
+
+  <audio src="" id="audio-source"></audio>
+
+  <!-- home section -->
+
+  <section class="home-section">
+    <!-- carousel -->
+    <div class="carousel">
+      <img v-for="(cover, index) in carouselCovers" :key="index"
+           :src="cover.cover"
+           :class="{'active': cover.isActive}"
+           :alt="cover.alt">
+    </div>
+
+    <!-- playlists -->
+    <h1 class="heading">recently played</h1>
+    <div class="playlists-group">
+      <div class="playlist-card">
+        <img src="./assets/covers/cover9.png" class="playlist-card-img" alt="">
+        <p class="playlist-card-name">top international</p>
+      </div>
+      <div class="playlist-card">
+        <img src="./assets/covers/cover2.png" class="playlist-card-img" alt="">
+        <p class="playlist-card-name">BTS collection</p>
+      </div>
+      <div class="playlist-card">
+        <img src="./assets/covers/cover10.png" class="playlist-card-img" alt="">
+        <p class="playlist-card-name">bollywood hits</p>
+      </div>
+      <div class="playlist-card">
+        <img src="./assets/covers/cover11.png" class="playlist-card-img" alt="">
+        <p class="playlist-card-name">top 50</p>
+      </div>
+      <div class="playlist-card">
+        <img src="./assets/covers/cover5.png" class="playlist-card-img" alt="">
+        <p class="playlist-card-name">justin biber</p>
+      </div>
+    </div>
+    <h1 class="heading">based on your listening</h1>
+    <div class="playlists-group">
+      <div class="playlist-card">
+        <img src="./assets/covers/cover11.png" class="playlist-card-img" alt="">
+        <p class="playlist-card-name">top international</p>
+      </div>
+      <div class="playlist-card">
+        <img src="./assets/covers/cover12.png" class="playlist-card-img" alt="">
+        <p class="playlist-card-name">BTS collection</p>
+      </div>
+      <div class="playlist-card">
+        <img src="./assets/covers/cover13.png" class="playlist-card-img" alt="">
+        <p class="playlist-card-name">bollywood hits</p>
+      </div>
+      <div class="playlist-card">
+        <img src="./assets/covers/cover1.png" class="playlist-card-img" alt="">
+        <p class="playlist-card-name">top 50</p>
+      </div>
+      <div class="playlist-card">
+        <img src="./assets/covers/cover3.png" class="playlist-card-img" alt="">
+        <p class="playlist-card-name">justin biber</p>
+      </div>
+    </div>
+  </section>
+
+<!--Music player section-->
+  <section class="music-player-section" @click="toggleMusicPlayer(true)"
+           :class="{'active': isMusicPlayerActive}">
+
+    <img src="./assets/covers/back.png" class="back-btn cursor-pointer icon hide" alt="back button"
+         @click.stop="toggleMusicPlayer(false)">
+    <img src="./assets/covers/nav.png" class="nav-btn cursor-pointer icon hide" alt="" @click="togglePlaylist(true)">
+
+    <h1 class="current-song-name">song 1</h1>
+    <p class="artist-name hide">artist 1</p>
+
+    <img src="./assets/covers/cover1.png" class="cover hide" alt="">
+
+    <div class="seek-bar-container">
+      <input type="range" class="music-seek-bar" value="0">
+      <p class="current-time hide">00 : 00</p>
+      <p class="duration hide">00 : 00</p>
+    </div>
+
+    <div class="controls">
+      <span class="fas fa-redo"></span>
+      <div class="main">
+        <i class="fas fa-backward active"></i>
+        <i class="fas fa-play active"></i>
+        <i class="fas fa-pause"></i>
+        <i class="fas fa-forward active"></i>
+      </div>
+      <input type="range" class="volume-slider" :class="{'active': isVolumeSliderActive}" max="1" value="1" step="0.1">
+      <span class="fas fa-volume-up cursor-pointer" :class="{'active': isVolumeSliderActive}" @click.stop="toggleVolumeSlider"></span>
+    </div>
+
+  </section>
+
+<!--Playlist section-->
+  <section class="playlist" :class="{'active': isPlaylistActive}">
+
+    <img src="./assets/covers/back.png" class="back-btn icon cursor-pointer"
+         @click="togglePlaylist(false)" alt="">
+
+    <h1 class="title">playlist</h1>
+
+    <div class="queue active">volumeBtn
+      <div class="queue-cover">
+        <img src="./assets/covers/cover1.png" alt="">
+        <i class="fas fa-pause"></i>
+      </div>
+      <p class="name">song 1</p>
+    </div>
+    <div class="queue">
+      <div class="queue-cover">
+        <img src="./assets/covers/cover2.png" alt="">
+        <i class="fas fa-pause"></i>
+      </div>
+      <p class="name">song 2</p>
+    </div>
+    <div class="queue">
+      <div class="queue-cover">
+        <img src="./assets/covers/cover3.png" alt="">
+        <i class="fas fa-pause"></i>
+      </div>
+      <p class="name">song 3</p>
+    </div>
+    <div class="queue">
+      <div class="queue-cover">
+        <img src="./assets/covers/cover4.png" alt="">
+        <i class="fas fa-pause"></i>
+      </div>
+      <p class="name">song 4</p>
+    </div>
+    <div class="queue">
+      <div class="queue-cover">
+        <img src="./assets/covers/cover5.png" alt="">
+        <i class="fas fa-pause"></i>
+      </div>
+      <p class="name">song 5</p>
+    </div>
+    <div class="queue">
+      <div class="queue-cover">
+        <img src="./assets/covers/cover6.png" alt="">
+        <i class="fas fa-pause"></i>
+      </div>
+      <p class="name">song 6</p>
+    </div>
+    <div class="queue">
+      <div class="queue-cover">
+        <img src="./assets/covers/cover7.png" alt="">
+        <i class="fas fa-pause"></i>
+      </div>
+      <p class="name">song 7</p>
+    </div>
+    <div class="queue">
+      <div class="queue-cover">
+        <img src="./assets/covers/cover8.png" alt="">
+        <i class="fas fa-pause"></i>
+      </div>
+      <p class="name">song 8</p>
+    </div>
+
+  </section>
 </template>
 
 <script>
-
+import {songs} from '/src/data/songs'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      allSongs: [],
+      carouselCovers: [],
+      carouselCoverIndex: 0,
+      isMusicPlayerActive: false,
+      isPlaylistActive: false,
+      isVolumeSliderActive: false
+    }
+  },
+  created() {
+    this.allSongs = [...songs]
+    this.carouselCovers = this.allSongs.map((s, ind) => {
+      return {
+        cover: s.cover,
+        alt: `Cover${ind+1}`,
+        isActive: ind === 0 ? true : false // make the first cover as active in the beginning
+      }
+    })
+  },
+  mounted() {
+    // set interval to change the cover carousel
+    setInterval(() => {
+      this.changeCarousel();
+    }, 3000);
+  },
+  methods: {
+    // method for continously changing the cover on home section within 3 secs interval
+    changeCarousel() {
+      // set the previously active cover as inactive
+      this.carouselCovers[this.carouselCoverIndex].isActive = false
+
+      if(this.carouselCoverIndex >= this.carouselCovers.length - 1){
+        this.carouselCoverIndex = 0;
+      } else{
+        this.carouselCoverIndex++;
+      }
+      // set the next carousel as active
+      this.carouselCovers[this.carouselCoverIndex].isActive = true
+    },
+    toggleMusicPlayer(bool) {
+      this.isMusicPlayerActive = bool
+      this.isVolumeSliderActive = false
+    },
+    togglePlaylist(bool) {
+      this.isPlaylistActive = bool
+      this.isVolumeSliderActive = false
+    },
+    toggleVolumeSlider() {
+      this.isVolumeSliderActive = !this.isVolumeSliderActive
+    }
+  }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
 
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:root{
+  --background: #141414;
+  --text-color: #fff;
+  --primary-color: #63ff69;
+  --secondary-color: #000;
+  --alpha-color: rgba(0, 0, 0, 0.5);
+  --shadow: 0 15px 40px var(--alpha-color);
+}
+
+html{
+  background: var(--background);
+  display: flex;
+  justify-content: center;
+}
+
+body{
+  width: 100%;
+  height: 100vh;
+  max-width: 375px;
+  position: relative;
+  background: var(--background);
+  font-family: 'roboto', sans-serif;
+  color: var(--text-color);
+}
+
+::-webkit-scrollbar{
+  display: none;
+}
+
+/* home section */
+
+.home-section{
+  width: 100%;
+  padding: 20px;
+  height: 100%;
+  padding-bottom: 100px;
+  overflow-y: auto;
+}
+
+/* carousel */
+
+.carousel{
+  width: 100%;
+  height: 200px;
+  overflow: hidden;
+  border-radius: 20px;
+  box-shadow: var(--shadow);
+  position: relative;
+}
+
+.carousel img{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  opacity: 0;
+  transition: 1s;
+}
+
+.carousel img.active{
+  opacity: 1;
+}
+
+.heading{
+  margin: 30px 0 10px;
+  text-transform: capitalize;
+  font-weight: 400;
+  font-size: 30px;
+}
+
+/* playlists card */
+
+.playlists-group{
+  position: relative;
+  width: 100%;
+  min-height: 200px;
+  height: auto;
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+}
+
+.playlist-card{
+  flex: 0 0 auto;
+  max-width: 150px;
+  height: 100%;
+  margin-right: 20px;
+}
+
+.playlist-card-img{
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 20px;
+}
+
+.playlist-card-name{
+  width: 100%;
+  text-align: justify;
+  font-size: 20px;
+  text-transform: capitalize;
+  padding: 5px;
+}
+
+/* music player */
+
+/* minimize view */
+
+.music-player-section{
+  width: 100%;
+  height: 100px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  background: var(--alpha-color);
+  backdrop-filter: blur(50px);
+  transition: 1s;
+}
+
+.music-seek-bar{
+  -webkit-appearance: none;
+  width: 100%;
+  position: absolute;
+  top: -4px;
+  height: 8px;
+  background: var(--secondary-color);
+  overflow: hidden;
+}
+
+.music-seek-bar::-webkit-slider-thumb{
+  -webkit-appearance: none;
+  height: 10px;
+  width: 5px;
+  background: var(--primary-color);
+  cursor: pointer;
+  box-shadow: -400px 0 0 400px var(--primary-color);
+}
+
+.current-song-name{
+  font-weight: 300;
+  font-size: 20px;
+  text-align: center;
+  margin-top: 5px;
+  text-transform: capitalize;
+}
+
+.controls{
+  position: relative;
+  width: 80%;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+  font-size: 30px;
+}
+
+.controls span{
+  display: none;
+  opacity: 0;
+  transition: 1s;
+}
+
+.music-player-section.active .controls{
+  justify-content: space-between;
+}
+
+.music-player-section.active .controls span{
+  font-size: 25px;
+  display: block;
+  opacity: 0.5;
+}
+
+.music-player-section.active .controls span.active{
+  color: var(--primary-color);
+  opacity: 1;
+}
+
+.controls .main i{
+  margin: 0 5px;
+  display: none;
+}
+
+.controls .main i.active{
+  display: inline;
+}
+
+/* maximize music player styles */
+
+.music-player-section .hide{
+  display: none;
+  opacity: 0;
+  transition: 1s;
+}
+
+.music-player-section.active .hide{
+  display: block;
+  opacity: 1;
+}
+
+.music-player-section.active{
+  width: 100%;
+  height: 100%;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+}
+
+.music-player-section.active .music-seek-bar{
+  position: relative;
+  display: block;
+  border-radius: 50px;
+  margin: auto;
+}
+
+.music-player-section.active .current-song-name{
+  font-size: 40px;
+}
+
+.music-player-section.active .controls{
+  width: 100%;
+  font-size: 50px;
+}
+
+.artist-name{
+  text-align: center;
+  font-size: 20px;
+  text-transform: capitalize;
+}
+
+.cover{
+  width: 30vh;
+  height: 30vh;
+  object-fit: cover;
+  margin: auto;
+  border-radius: 20px;
+  box-shadow: var(--shadow);
+}
+
+.current-time{
+  position: absolute;
+  margin-top: 5px;
+  left: 30px;
+}
+
+.duration{
+  position: absolute;
+  margin-top: 5px;
+  right: 30px;
+}
+
+.icon{
+  position: absolute;
+  top: 60px;
+  transform: scale(1.3);
+}
+
+.back-btn{
+  left: 40px;
+}
+
+.nav-btn{
+  right: 40px;
+}
+
+/* volume button */
+
+.volume-slider{
+  -webkit-appearance: none;
+  width: 100px;
+  height: 40px;
+  position: absolute;
+  right: -35px;
+  bottom: 80px;
+  transform: rotate(-90deg);
+  border-radius: 20px;
+  background: var(--alpha-color);
+  overflow: hidden;
+  opacity: 0;
+  display: none;
+}
+
+.volume-slider.active{
+  opacity: 1;
+  display: block;
+}
+
+.volume-slider::-webkit-slider-thumb{
+  -webkit-appearance: none;
+  height: 40px;
+  width: 10px;
+  background: var(--primary-color);
+  box-shadow: -200px 0 1px 200px var(--primary-color);
+}
+
+/* playlist section */
+
+.playlist{
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  right: -100%;
+  padding: 30px 0;
+  background: var(--background);
+  z-index: 3;
+  transition: 1s;
+  overflow: auto;
+}
+
+.playlist.active{
+  right: 0;
+}
+
+.title{
+  font-weight: 300;
+  font-size: 40px;
+  text-align: center;
+  margin-top: 15px;
+  text-transform: capitalize;
+  margin-bottom: 30px;
+}
+
+.queue{
+  width: 100%;
+  height: 80px;
+  padding: 0 30px;
+  display: flex;
+  align-items: center;
+  border-top: 2px solid var(--alpha-color);
+}
+
+.queue-cover{
+  width: 60px;
+  height: 60px;
+  border-radius: 10px;
+  overflow: hidden;
+  margin-right: 20px;
+  position: relative;
+}
+
+.queue-cover img{
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.queue-cover i{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 30px;
+  color: var(--primary-color);
+  display: none;
+}
+
+.queue.active i{
+  display: block;
+}
+
+.queue .name{
+  font-size: 22px;
+  text-transform: capitalize;
+}
+.cursor-pointer {
+  cursor: pointer;
+}
 </style>
---
+++ src/data/songs.js
@@ -0,0 +1,80 @@
+export const songs = [
+  {
+    name: 'Song 1',
+    path: './assets/songs/Song1.mp3',
+    artist: 'Artist 1',
+    cover: './assets/covers/cover1.png'
+  },
+  {
+    name: 'Song 2',
+    path: './assets/songs/Song2.mp3',
+    artist: 'Artist 2',
+    cover: './assets/covers/cover2.png'
+  },
+  {
+    name: 'Song 3',
+    path: './assets/songs/Song3.mp3',
+    artist: 'Artist 3',
+    cover: './assets/covers/cover3.png'
+  },
+  {
+    name: 'Song 4',
+    path: './assets/songs/Song4.mp3',
+    artist: 'Artist 4',
+    cover: './assets/covers/cover4.png'
+  },
+  {
+    name: 'Song 5',
+    path: './assets/songs/Song5.mp3',
+    artist: 'Artist 5',
+    cover: './assets/covers/cover5.png'
+  },
+  {
+    name: 'Song 6',
+    path: './assets/songs/Song6.mp3',
+    artist: 'Artist 6',
+    cover: './assets/covers/cover6.png'
+  },
+  {
+    name: 'Song 7',
+    path: './assets/songs/Song7.mp3',
+    artist: 'Artist 7',
+    cover: './assets/covers/cover7.png'
+  },
+  {
+    name: 'Song 8',
+    path: './assets/songs/Song8.mp3',
+    artist: 'Artist 8',
+    cover: './assets/covers/cover8.png'
+  },
+  {
+    name: 'Song 9',
+    path: './assets/songs/Song9.mp3',
+    artist: 'Artist 9',
+    cover: './assets/covers/cover9.png'
+  },
+  {
+    name: 'Song 10',
+    path: './assets/songs/Song10.mp3',
+    artist: 'Artist 10',
+    cover: './assets/covers/cover10.png'
+  },
+  {
+    name: 'Song 11',
+    path: './assets/songs/Song11.mp3',
+    artist: 'Artist 11',
+    cover: './assets/covers/cover11.png'
+  },
+  {
+    name: 'Song 12',
+    path: './assets/songs/Song12.mp3',
+    artist: 'Artist 12',
+    cover: './assets/covers/cover12.png'
+  },
+  {
+    name: 'Song 13',
+    path: './assets/songs/Song13.mp3',
+    artist: 'Artist 13',
+    cover: './assets/covers/cover13.png'
+  },
+]
