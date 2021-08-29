957fab726bdc2a0e752817c6a646583b205fe641
keithmchd48 <keithmchd48@gmail.com>
2021-08-29 15:21:05 +0530

    path changed to /assests/...

--- src/data/songs.js
+++ src/data/songs.js
@@ -1,79 +1,79 @@
 export const songs = [
   {
     name: 'A window to the past',
-    path: './assets/songs/Song1.mp3',
+    path: '/assets/songs/Song1.mp3',
     artist: 'John Williams',
-    cover: './assets/covers/cover1.jpeg',
+    cover: '/assets/covers/cover1.jpeg',
     alt: 'Cover 1'
   },
   {
     name: 'Come on Eileen',
-    path: './assets/songs/Song2.mp3',
+    path: '/assets/songs/Song2.mp3',
     artist: 'Dexys midnight runners',
-    cover: './assets/covers/cover2.jpg',
+    cover: '/assets/covers/cover2.jpg',
     alt: 'Cover 2'
   },
   {
     name: 'Como los olivos',
-    path: './assets/songs/Song3.mp3',
+    path: '/assets/songs/Song3.mp3',
     artist: 'Bebe',
-    cover: './assets/covers/cover3.jpg',
+    cover: '/assets/covers/cover3.jpg',
     alt: 'Cover 3'
   },
   {
     name: 'Heroes',
-    path: './assets/songs/Song4.mp3',
+    path: '/assets/songs/Song4.mp3',
     artist: 'David Bowie',
-    cover: './assets/covers/cover4.png',
+    cover: '/assets/covers/cover4.png',
     alt: 'Cover 4'
   },
   {
     name: 'Nobody but me',
-    path: './assets/songs/Song5.mp3',
+    path: '/assets/songs/Song5.mp3',
     artist: 'The Human Beinz',
-    cover: './assets/covers/cover5.jpeg',
+    cover: '/assets/covers/cover5.jpeg',
     alt: 'Cover 5'
   },
   {
     name: 'O Children',
-    path: './assets/songs/Song6.mp3',
+    path: '/assets/songs/Song6.mp3',
     artist: 'Nick Cave & the bad seeds',
-    cover: './assets/covers/cover6.jpeg',
+    cover: '/assets/covers/cover6.jpeg',
     alt: 'Cover 6'
   },
   {
     name: 'Siempre me quedara',
-    path: './assets/songs/Song7.mp3',
+    path: '/assets/songs/Song7.mp3',
     artist: 'Bebe',
-    cover: './assets/covers/cover7.jpeg',
+    cover: '/assets/covers/cover7.jpeg',
     alt: 'Cover 7'
   },
   {
     name: 'The other side',
-    path: './assets/songs/Song8.mp3',
+    path: '/assets/songs/Song8.mp3',
     artist: 'Hugh Jackman & Zac Efron',
-    cover: './assets/covers/cover8.jpeg',
+    cover: '/assets/covers/cover8.jpeg',
     alt: 'Cover 8'
   },
   {
     name: 'Femme like you',
-    path: './assets/songs/Song9.mp3',
+    path: '/assets/songs/Song9.mp3',
     artist: 'Monaldin feat. emma peters',
-    cover: './assets/covers/cover9.jpeg',
+    cover: '/assets/covers/cover9.jpeg',
     alt: 'Cover 9'
   },
   {
     name: 'Honeypie',
-    path: './assets/songs/Song10.mp3',
+    path: '/assets/songs/Song10.mp3',
     artist: 'JAWNY',
-    cover: './assets/covers/cover10.jpeg',
+    cover: '/assets/covers/cover10.jpeg',
     alt: 'Cover 10'
   },
   {
     name: 'Rolling in the deep',
-    path: './assets/songs/Song11.mp3',
+    path: '/assets/songs/Song11.mp3',
     artist: 'Adele',
-    cover: './assets/covers/cover11.png',
+    cover: '/assets/covers/cover11.png',
     alt: 'Cover 11'
   }
 ]
